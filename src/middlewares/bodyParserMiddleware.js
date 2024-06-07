6327e72b0bfcdd52ca13246ff0e7c0ef6cfc6c72
arya2004 <arya20j4@gmail.com>
2024-06-07 12:28:18 +0530

    docs: middleware

--- src/middlewares/bodyParserMiddleware.js
+++ src/middlewares/bodyParserMiddleware.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+/**
+ * Middleware to parse JSON bodies of incoming requests.
+ */
 app.use(bodyParser.json());
 
 module.exports = app;
