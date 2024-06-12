b5f640db01b9707fc8df153af1cfe7d6c946746b
arya2004 <arya20j4@gmail.com>
2024-06-12 11:58:09 +0530

    ft: change return json key

--- src/controllers/singleSpawnController.js
+++ src/controllers/singleSpawnController.js
@@ -26,7 +26,7 @@ const createRouter = (redis) => {
             const ip = req.ip;
             redis.set(ip, 'blacklisted', 'EX', 60);
             console.log(`IP ${ip} blacklisted for posting a slur.`);
-            return res.status(403).json({ error: 'You are blacklisted for posting a slur.' });
+            return res.status(403).json({ message: 'You are blacklisted for posting a slur.' });
         }
 
         next();
@@ -57,13 +57,13 @@ const createRouter = (redis) => {
             const isBlacklisted = await redis.get(req.ip);
             if (isBlacklisted === 'blacklisted') {
                 console.warn(`Blacklisted IP ${req.ip} attempted to post.`);
-                return res.status(403).json({ error: 'You are blacklisted for posting a slur.' });
+                return res.status(403).json({ message: 'You are blacklisted for posting malicious code.' });
             }
 
             const game = await Game.findOne({ taskNo });
             if (!game) {
                 console.warn(`Task not found for taskNo: ${taskNo}`);
-                return res.status(404).json({ error: 'Task not found' });
+                return res.status(404).json({ message: 'Task not found' });
             }
 
             let combinedCode = game.masterCodeBoilerplate;
@@ -117,7 +117,7 @@ const createRouter = (redis) => {
             res.status(200).json({ userCodeOutput, success });
         } catch (error) {
             console.error(`Error processing request: ${error}`);
-            res.status(500).json({ error: 'Internal server error' });
+            res.status(500).json({ message: 'Internal server error', error });
         }
     });
 
